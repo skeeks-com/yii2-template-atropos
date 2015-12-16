bf11e004fea0a90c3d6f3e553f9398b068259e43
Semenov <semenov@skeeks.com>
2015-12-16 18:23:10 +0300

    start

---
+++ AtroposAsset.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @author Semenov Alexander <semenov@skeeks.com>
+ * @link http://skeeks.com/
+ * @copyright 2010 SkeekS (ÑêèêÑ)
+ * @date 24.10.2015
+ */
+namespace skeeks\template\atropos;
+use yii\web\AssetBundle;
+class AtroposAsset extends AssetBundle
+{
+    public $sourcePath = '@skeeks/template/atropos/src/';
+    /**
+     * @param string $asset
+     * @return string
+     * @throws \yii\base\InvalidConfigException
+     */
+    static public function getAssetUrl($asset)
+    {
+        return \Yii::$app->assetManager->getAssetUrl(\Yii::$app->assetManager->getBundle(static::className()), $asset);
+    }
+}
